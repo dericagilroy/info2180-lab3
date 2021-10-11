b4320f3746547cef9b637cdbcbbdb369a37a9f54
dericagilroy <dericagilroy24@gmail.com>
2021-10-10 22:32:29 -0500

    Edited tic-tac-toe.js; includes code that determines the winner of the game and gives a status message

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,6 +4,7 @@ const gameState = {};
 let last = "X";
 let len;
 let squares;
+let statusArea;
 
 window.addEventListener("DOMContentLoaded", function() {boardLayout(); addText(); addHover()});
 
@@ -44,6 +45,7 @@ function manageState(event) {
     gameState[event.target.attributes.id.value] = char;
     event.target.textContent = char;
     event.target.classList.toggle(char);
+    determineWinner();
 }
 
 function addHover() {
@@ -56,4 +58,31 @@ function addHover() {
 
 function hover(event) {
     event.target.classList.toggle("hover");
+}
+
+function determineWinner() {
+    statusArea = document.getElementById("status");
+
+    if ((gameState[0] == gameState[1] && gameState[1] == gameState[2] && gameState[2]== "X") || (gameState[3] == gameState[4] && gameState[4] == gameState[5] && gameState[5] == "X") || 
+       (gameState[6] == gameState[7] && gameState[7] == gameState[8] && gameState[8] == "X") || (gameState[0] == gameState[3] && gameState[3] == gameState[6] && gameState[6] == "X") ||
+       (gameState[1] == gameState[4] && gameState[4] == gameState[7] && gameState[7] == "X") || (gameState[2] == gameState[5] && gameState[5] == gameState[8] && gameState[8] == "X") ||
+       (gameState[0] == gameState[4] && gameState[4] == gameState[8] && gameState[8] == "X") || (gameState[2] == gameState[4] && gameState[4] == gameState[6] && gameState[6] == "X")) {
+           statusArea.textContent = "Congratulations! X is the Winner!";
+           statusArea.setAttribute("class", "you-won");
+    }
+
+    else if ((gameState[0] == gameState[1] && gameState[1] == gameState[2] && gameState[2]== "O") || (gameState[3] == gameState[4] && gameState[4] == gameState[5] && gameState[5] == "O") || 
+            (gameState[6] == gameState[7] && gameState[7] == gameState[8] && gameState[8] == "O") || (gameState[0] == gameState[3] && gameState[3] == gameState[6] && gameState[6] == "O") ||
+            (gameState[1] == gameState[4] && gameState[4] == gameState[7] && gameState[7] == "O") || (gameState[2] == gameState[5] && gameState[5] == gameState[8] && gameState[8] == "O") ||
+            (gameState[0] == gameState[4] && gameState[4] == gameState[8] && gameState[8] == "O") || (gameState[2] == gameState[4] && gameState[4] == gameState[6] && gameState[6] == "O")) {
+                statusArea.textContent = "Congratulations! O is the Winner!"; 
+                statusArea.setAttribute("class", "you-won");
+    }
+    else if (len == 8) {
+        statusArea.textContent = "There is a tie!";
+    }
+    else {
+        statusArea.textContent = "Move your mouse over a square and click to play an X or an O.";
+        statusArea.classList.remove("you-won");
+    }
 }
