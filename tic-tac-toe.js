7d6f03856171cb0dc6e09e818d1d4eab33b674a5
dericagilroy <dericagilroy24@gmail.com>
2021-10-10 22:37:28 -0500

    Edited tic-tac-toe.js; includes code that prevents players from cheating through changing cell values

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -27,25 +27,34 @@ function manageState(event) {
     let char;
     len = Object.keys(gameState).length;
 
-    if (len == 0) {
-        char = "X";
-        last = "X"; 
+    if (event.target.textContent != "") {
+        this.removeEventListener("click", manageState);
     }
-    else if (len > 0) {
-        if (last == "X") {
-            char = "O"
-            last = "O";
-        }
-        else {
+    else {
+        if (len == 0) {
             char = "X";
-            last = "X";
+            last = "X"; 
         }
+        else if (len > 0) {
+            if (last == "X") {
+                char = "O"
+                last = "O";
+            }
+            else {
+                char = "X";
+                last = "X";
+            }
+        }
+
+        gameState[event.target.attributes.id.value] = char;
+        event.target.textContent = char;
+        event.target.classList.toggle(char);
+        determineWinner();
     }
 
-    gameState[event.target.attributes.id.value] = char;
-    event.target.textContent = char;
-    event.target.classList.toggle(char);
-    determineWinner();
+    event.target.classList.remove("hover");
+    event.target.removeEventListener("mouseover", hover);
+    event.target.removeEventListener("mouseout", hover);
 }
 
 function addHover() {
@@ -69,6 +78,13 @@ function determineWinner() {
        (gameState[0] == gameState[4] && gameState[4] == gameState[8] && gameState[8] == "X") || (gameState[2] == gameState[4] && gameState[4] == gameState[6] && gameState[6] == "X")) {
            statusArea.textContent = "Congratulations! X is the Winner!";
            statusArea.setAttribute("class", "you-won");
+
+            squares = [...document.getElementById("board").children];
+            for (const div of squares) {
+                div.removeEventListener("mouseover", hover);
+                div.removeEventListener("mouseout", hover);
+                div.removeEventListener("click", manageState);
+            }
     }
 
     else if ((gameState[0] == gameState[1] && gameState[1] == gameState[2] && gameState[2]== "O") || (gameState[3] == gameState[4] && gameState[4] == gameState[5] && gameState[5] == "O") || 
@@ -77,13 +93,18 @@ function determineWinner() {
             (gameState[0] == gameState[4] && gameState[4] == gameState[8] && gameState[8] == "O") || (gameState[2] == gameState[4] && gameState[4] == gameState[6] && gameState[6] == "O")) {
                 statusArea.textContent = "Congratulations! O is the Winner!"; 
                 statusArea.setAttribute("class", "you-won");
-    }
-    else if (len == 8) {
-        statusArea.textContent = "There is a tie!";
+                
+                squares = [...document.getElementById("board").children];
+                for (const div of squares) {
+                    div.removeEventListener("mouseover", hover);
+                    div.removeEventListener("mouseout", hover);
+                    div.removeEventListener("click", manageState);
+                }
     }
     else {
-        statusArea.textContent = "Move your mouse over a square and click to play an X or an O.";
-        statusArea.classList.remove("you-won");
+        if (len == 8) {
+            statusArea.textContent = "There is a tie!";
+        }
     }
 }
 
